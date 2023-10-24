d5e2cedba5c93fc067f1bd2dcd46ddd65451ca56
amey-patki <patkiamey09@gmail.com>
2023-10-24 23:39:09 +0530

    commit 1 for 24th oct

---
+++ Python_programs/Day-2/07_float.py
@@ -0,0 +1,19 @@
+#float values
+
+number1= 2.2
+number2=5.0
+
+result1=number1+number2
+print(result1)
+
+result2=number2-number1
+print(result2)
+
+result3=number1*number2
+print(result3)
+
+result4=number1/number2
+print(result4)
+
+result5=round(3.14592578654 , 2)
+print(result5)
