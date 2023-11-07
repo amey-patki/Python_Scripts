1fb35044292e0b7dc955a444d0973003a4ccb291
Amey Patki <53407006+amey-patki@users.noreply.github.com>
2023-11-07 14:22:05 +0000

    backup script

---
+++ Programs/Backup.py
@@ -0,0 +1,20 @@
+import shutil
+import datetime
+
+# Define the source directory to be backed up
+source_dir = "/workspaces/Python_Scripts/Programs"
+
+# Create a timestamp for the backup folder
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+backup_dir = f"/workspaces/Python_Scripts/Backup_{timestamp}"
+
+try:
+    # Copy the contents of the source directory to the backup directory
+    shutil.copytree(source_dir, backup_dir)
+    print(f"Backup created successfully at {backup_dir}")
+except FileNotFoundError:
+    print(f"Source directory not found.")
+except FileExistsError:
+    print(f"Backup directory already exists.")
+except Exception as e:
+    print(f"An error occurred: {e}")
