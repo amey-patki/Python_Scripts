a757d237a578bc646d245ab2cfd64416f447895f
Amey Patki <53407006+amey-patki@users.noreply.github.com>
2023-11-07 14:33:35 +0000

    commit 6

--- Programs/Backup.py
+++ Programs/Backup.py
@@ -1,4 +1,4 @@
-import shutil
+import shutil # shutil module simplifies common file and directory tasks, making it easier to work with the file system.
 import datetime
 
 # Define the source directory to be backed up
@@ -6,7 +6,7 @@ source_dir = "/path/to/source_directory" #replace with your directory path for w
 
 # Create a timestamp for the backup folder
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-backup_dir = f"/path/to/backup_directory/backup_{timestamp}"
+backup_dir = f"/path/to/backup_directory/backup_{timestamp}" #backup folder
 
 try:
     # Copy the contents of the source directory to the backup directory
@@ -18,3 +18,5 @@ except FileExistsError:
     print(f"Backup directory already exists.")
 except Exception as e:
     print(f"An error occurred: {e}")
+
+
