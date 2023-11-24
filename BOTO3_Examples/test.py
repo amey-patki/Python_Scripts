fc7ff7fa0cc45493216a6920a872f43df2a7c22b
Amey Patki <53407006+amey-patki@users.noreply.github.com>
2023-11-24 18:23:30 +0000

    added boto3 folder and ex

---
+++ BOTO3_Examples/test.py
@@ -0,0 +1,15 @@
+import boto3
+
+# Connect to EC2
+ec2_client = boto3.client('ec2')
+
+# Start an EC2 instance
+response = ec2_client.start_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+
+# Stop an EC2 instance
+#response = ec2_client.stop_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+
+# Terminate an EC2 instance
+#response = ec2_client.terminate_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+
+
