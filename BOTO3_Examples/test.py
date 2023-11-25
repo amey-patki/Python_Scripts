ed41f9d2b838b40662647381652e5f623cbc6a78
amey-patki <patkiamey09@gmail.com>
2023-11-25 22:05:51 +0530

    Feat: boto3 module to start stop and terminate

--- BOTO3_Examples/test.py
+++ BOTO3_Examples/test.py
@@ -7,9 +7,9 @@ ec2_client = boto3.client('ec2')
 response = ec2_client.start_instances(InstanceIds=['i-06e7e79154c9abdf1'])
 
 # Stop an EC2 instance
-#response = ec2_client.stop_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+response = ec2_client.stop_instances(InstanceIds=['i-06e7e79154c9abdf1'])
 
 # Terminate an EC2 instance
-#response = ec2_client.terminate_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+response = ec2_client.terminate_instances(InstanceIds=['i-06e7e79154c9abdf1'])
 
 
