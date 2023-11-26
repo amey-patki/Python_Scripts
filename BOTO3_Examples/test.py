39ea5e5f090868da675ee0812007ba24da79fc34
amey-patki <patkiamey09@gmail.com>
2023-11-26 20:52:23 +0530

    Updated test.py

--- BOTO3_Examples/test.py
+++ BOTO3_Examples/test.py
@@ -4,12 +4,12 @@ import boto3
 ec2_client = boto3.client('ec2')
 
 # Start an EC2 instance
-response = ec2_client.start_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+response = ec2_client.start_instances(InstanceIds=['Your_Instance_ID'])
 
 # Stop an EC2 instance
-response = ec2_client.stop_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+response = ec2_client.stop_instances(InstanceIds=['Your_Instance_ID'])
 
 # Terminate an EC2 instance
-response = ec2_client.terminate_instances(InstanceIds=['i-06e7e79154c9abdf1'])
+response = ec2_client.terminate_instances(InstanceIds=['Your_Instance_ID'])
 
 
