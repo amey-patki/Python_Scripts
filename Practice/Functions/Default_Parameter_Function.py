57fd72e11ce26227bace4e3b07aa2a402d226878
amey-patki <patkiamey09@gmail.com>
2023-12-13 19:12:09 +0530

    updated file

--- Practice/Functions/Default_Parameter_Function.py
+++ Practice/Functions/Default_Parameter_Function.py
@@ -1,7 +1,20 @@
-def greet(name):
+'''def greet(name):
     print("Hello, " + name + "!")
     # Uses the default parameter
-greet("Alice")  
+greet("Alice")'''
+
+def addition(a,b):
+    result=a+b
+    return result
+
+
+Final_result=addition(3,4)
+print("Sum is :" , Final_result)
+    
+
+
+    
+
     
     
     
