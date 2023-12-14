eb07ca1d3256c7ba847b78887b71ec9ae78063f2
amey-patki <patkiamey09@gmail.com>
2023-12-14 19:13:53 +0530

    Feat: lambda function

---
+++ Practice/Functions/lambda_function.py
@@ -0,0 +1,7 @@
+# Lambda functions are often used for short, simple operations where a full function definition 
+# using the def keyword might be more than necessary.
+
+square = lambda x: x ** 2
+print("Square:", square(5))
+
+
