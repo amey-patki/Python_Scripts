bc815373a3b1de4c419719886bdf493473ad121f
amey-patki <patkiamey09@gmail.com>
2025-01-15 23:48:36 +0530

    First Commit

---
+++ DevOps/packages.py
@@ -0,0 +1,8 @@
+def install_package(package_name):
+    try:
+        # Install Python packages using pip
+        print(f"Installing {package_name}...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+        print(f'{package_name} installed successfully.')
+    except subprocess.CalledProcessError:
+        print(f'Failed to install {package_name}')
