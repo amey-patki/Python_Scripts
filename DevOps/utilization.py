01f2fb17cd9a3ecd20be1b09c703d577d66cc3c9
amey-patki <patkiamey09@gmail.com>
2025-01-17 22:25:12 +0530

    disk_usage python script

---
+++ DevOps/utilization.py
@@ -0,0 +1,12 @@
+import psutil
+
+def check_disk_usage():
+    disk_usage=psutil.disk_usage('/')
+    used_percent=disk_usage.percent
+    
+    print(f"Disk usage: {used_percent}%")
+    
+    return used_percent
+
+if __name__ == "__main__":
+    check_disk_usage()
