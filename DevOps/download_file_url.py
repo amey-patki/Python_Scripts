b5e39e93110013ff6f1070cc8dbdb59eabd2e075
amey-patki <patkiamey09@gmail.com>
2025-01-21 23:14:52 +0530

    updated script

--- DevOps/download_file_url.py
+++ DevOps/download_file_url.py
@@ -1,3 +1,23 @@
+# import requests
+
+# def download_file(url, local_filename):
+#     print(f"Downloading file from {url}...")
+#     response = requests.get(url)
+    
+#     # Check if the request was successful
+#     if response.status_code == 200:
+#         with open(local_filename, 'wb') as f:
+#             f.write(response.content)
+#         print(f"Downloaded {local_filename}")
+#     else:
+#         print(f"Failed to download file. Status code: {response.status_code}")
+
+# # Example usage with a working URL
+# url = 'https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore'  # Example URL for a .gitignore file
+# local_filename = 'Python.gitignore'
+# download_file(url, local_filename)
+
+
 import requests
 
 def download_file(url, local_filename):
@@ -11,8 +31,7 @@ def download_file(url, local_filename):
         print(f"Downloaded {local_filename}")
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
-
-# Example usage with a working URL
-url = 'https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore'  # Example URL for a .gitignore file
-local_filename = 'Python.gitignore'
+        
+url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt'
+local_filename = 'iso_8859-1.txt'
 download_file(url, local_filename)
