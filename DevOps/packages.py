c66be4f37792d078351af39d6267df8b8df617d7
Amey Patki <53407006+amey-patki@users.noreply.github.com>
2025-01-23 22:38:44 +0530

    Update packages.py

--- DevOps/packages.py
+++ DevOps/packages.py
@@ -3,6 +3,6 @@ def install_package(package_name):
         # Install Python packages using pip
         print(f"Installing {package_name}...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
-        print(f'{package_name} installed successfully')
+        print(f'{package_name} installed successfully .')
     except subprocess.CalledProcessError:
         print(f'Failed to install {package_name}')
